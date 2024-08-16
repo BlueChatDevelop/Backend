2c766905bba634897aa72913911b1f6968b0d4c4
Yehor-Havrylenko <egor.dar1216@gmail.com>
2024-08-16 13:52:51 +0200

    Update cors api

--- API/Authorization/Program.cs
+++ API/Authorization/Program.cs
@@ -1,8 +1,20 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Разрешаем CORS для всех источников или конкретного источника
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder
+            .WithOrigins("http://localhost:8080") // Укажите нужный источник
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
+
+builder.Services.AddControllers();
 
 if (app.Environment.IsDevelopment())
 {
