d25e6d40a604c9565987a2e7a90ccd5a0e445af8
Yehor-Havrylenko <egor.dar1216@gmail.com>
2024-08-16 22:34:21 +0200

    fix node proxy

--- Docker/Node/server.js
+++ Docker/Node/server.js
@@ -4,24 +4,28 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// Разрешаем CORS для всех источников
 app.use(cors());
 
-// Проксирование запросов на /api/authorization/* к http://localhost:5000
-app.use('/api/authorization', createProxyMiddleware({
-    target: 'http://localhost:5000',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/api/authorization': '/api/authorization', // Переписываем URL для соответствия API
-    }
-}));
+app.use(
+    '/api/authorization',
+    createProxyMiddleware({
+        target: 'http://api:5000/api/authorization', 
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api/authorization': '/api/authorization', 
+        },
+        logLevel: 'debug',
+        onError: (err, req, res) => {
+            console.error('Proxy error:', err);
+            res.status(500).send('Proxy error occurred.');
+        }
+    })
+);
 
-// Пример маршрута для проверки работы сервера
 app.get('/', (req, res) => {
     res.send('Node.js сервер работает и проксирует запросы на .NET API!');
 });
 
-// Запуск сервера на порту 8000
 app.listen(8000, () => {
     console.log('Node.js сервер запущен на http://localhost:8000');
 });
